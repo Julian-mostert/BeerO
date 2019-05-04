d2aae1d96dd7e6c36a982b2a2d1f28251647b663
Julian Mostert <Julian.Mostert123@gmail.Com>
2019-05-05 00:31:57 +0200

    Update web socket connection, close and message

--- BeerO.SlackConnector/Connections/Sockets/IWebSocketClient.cs
+++ BeerO.SlackConnector/Connections/Sockets/IWebSocketClient.cs
@@ -7,13 +7,13 @@ namespace BeerO.SlackConnector.Connections.Sockets
 {
     internal interface IWebSocketClient
     {
-        //bool IsAlive { get; }
+        bool IsAlive { get; }
 
-        Task Connect(string webSockerUrl);
+        Task Connect(string          webSockerUrl);
         Task SendMessage(BaseMessage message);
         Task Close();
 
         event EventHandler<InboundMessage> OnMessage;
-        event EventHandler OnClose;
+        event EventHandler                 OnClose;
     }
 }
--- BeerO.SlackConnector/Connections/Sockets/WebSocketClientLite.cs
+++ BeerO.SlackConnector/Connections/Sockets/WebSocketClientLite.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Threading;
 using System.Threading.Tasks;
 using BeerO.SlackConnector.Connections.Sockets.Messages.Inbound;
 using BeerO.SlackConnector.Connections.Sockets.Messages.Outbound;
@@ -11,36 +14,67 @@ namespace BeerO.SlackConnector.Connections.Sockets
 {
     internal class WebSocketClientLite : IWebSocketClient
     {
-        private readonly IMessageInterpreter _interpreter;
-        private readonly List<IDisposable> _subscriptions = new List<IDisposable>();
-        private IMessageWebSocketRx _webSocket;
-        private int _currentMessageId;
+        private readonly IMessageInterpreter     _interpreter;
+        private          IMessageWebSocketRx     _webSocket;
+        private          int                     _currentMessageId;
+        private          CancellationTokenSource _outerCancelationSource;
+        private          IDisposable             _disposableWebsocketStatus;
+        private          IDisposable             _disposableMessageReceiver;
 
-        public bool IsAlive => this._webSocket.IsConnected;
+        public bool IsAlive => this._outerCancelationSource.IsCancellationRequested;
 
         public WebSocketClientLite(IMessageInterpreter interpreter)
         {
             this._interpreter = interpreter;
+            this._outerCancelationSource = new CancellationTokenSource();
         }
 
         public async Task Connect(string webSockerUrl)
         {
-            if (this._webSocket != null)
+            while (!this._outerCancelationSource.IsCancellationRequested)
             {
-                await this.Close();
+                var innerCancellationSource = new CancellationTokenSource();
+
+                await this.StartWebSocketAsync(innerCancellationSource, webSockerUrl);
+
+                while (!innerCancellationSource.IsCancellationRequested)
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(10), innerCancellationSource.Token);
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(5), this._outerCancelationSource.Token);
             }
+        }
 
-            this._webSocket = new MessageWebSocketRx();
-            this._subscriptions.Add(this._webSocket.ObserveConnectionStatus.Subscribe(OnConnectionChange));
+        private async Task StartWebSocketAsync(CancellationTokenSource innerCancellationTokenSource,
+                                               string webSocketUrl)
+        {
+            using (TcpClient tcpClient = new TcpClient { LingerState = new LingerOption(true, 0) })
+                this._webSocket = new MessageWebSocketRx(tcpClient)
+                {
+                    IgnoreServerCertificateErrors = true,
+                    Headers = new Dictionary<string, string>
+                    {
+                        { "Pragma", "no-cache" }, 
+                        { "Cache-Control", "no-cache" }
+                    },
+                    TlsProtocolType = SslProtocols.Tls12,
+                };
+            Console.WriteLine("Start");
 
-            var uri = new Uri(webSockerUrl);
-            var messageObserver = await this._webSocket.CreateObservableMessageReceiver(uri, excludeZeroApplicationDataInPong: true);
-            this._subscriptions.Add(messageObserver.Subscribe(OnWebSocketOnMessage));
+            this._disposableWebsocketStatus =
+                this._webSocket.ConnectionStatusObservable
+                    .Subscribe(this.OnConnectionChange(innerCancellationTokenSource));
+
+            this._disposableMessageReceiver =
+                this._webSocket.MessageReceiverObservable.Subscribe(this.OnWebSocketOnMessage());
+
+            await this._webSocket.ConnectAsync(new Uri(webSocketUrl));
         }
 
         public async Task SendMessage(BaseMessage message)
         {
-            System.Threading.Interlocked.Increment(ref this._currentMessageId);
+            Interlocked.Increment(ref this._currentMessageId);
             message.Id = this._currentMessageId;
             string json = JsonConvert.SerializeObject(message);
 
@@ -51,35 +85,42 @@ namespace BeerO.SlackConnector.Connections.Sockets
         {
             using (this._webSocket)
             {
-                foreach (var subscription in this._subscriptions)
-                {
-                    subscription.Dispose();
-                }
-                this._subscriptions.Clear();
-
-                await this._webSocket.CloseAsync();
+                this._disposableMessageReceiver.Dispose();
+                this._disposableWebsocketStatus.Dispose();
+                await this._webSocket.DisconnectAsync();
             }
         }
 
         public event EventHandler<InboundMessage> OnMessage;
-        private void OnWebSocketOnMessage(string message)
+
+        private Action<string> OnWebSocketOnMessage()
         {
-            string messageJson = message ?? "";
-            var inboundMessage = this._interpreter.InterpretMessage(messageJson);
-            this.OnMessage?.Invoke(this, inboundMessage);
+            return replyMessage =>
+            {
+                string messageJson = replyMessage ?? "";
+                var inboundMessage = this._interpreter.InterpretMessage(messageJson);
+                this.OnMessage?.Invoke(this, inboundMessage);
+            };
         }
 
         public event EventHandler OnClose;
-        private void OnConnectionChange(ConnectionStatus connectionStatus)
+
+        private Action<ConnectionStatus> OnConnectionChange(CancellationTokenSource innerCancellationTokenSource)
         {
-            switch (connectionStatus)
+            return status =>
             {
-                case ConnectionStatus.Aborted:
-                case ConnectionStatus.ConnectionFailed:
-                case ConnectionStatus.Disconnected:
-                    this.OnClose?.Invoke(this, null);
-                    break;
-            }
+                switch (status)
+                {
+                    case ConnectionStatus.Aborted:
+                    case ConnectionStatus.ConnectionFailed:
+                    case ConnectionStatus.Disconnected:
+                        this.OnClose?.Invoke(this, null);
+                        break;
+                }
+
+                innerCancellationTokenSource
+                    .Cancel();
+            };
         }
     }
 }
